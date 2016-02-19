77558a4886a18bbc1fb6bd996c230a75adcd7709
Mickaël Wolff <mickael@lupusmic.org>
2016-02-19 09:37:49 +0100

    Display error when Stat unavailable

--- message/views.py
+++ message/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import serializers
+from rest_framework import serializers, status
+
+from models import Stats
 
 def index():
     pass
+
+@api_view(['GET'])
+def stat(r):
+    try:
+        data = Stats.objects.get()
+        serializer = serializers.Serializer(data=data)
+        if serializer.is_valid():
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Stats.DoesNotExist:
+        payload = { 'result':'error', 'error':'Stat unavailable' }
+        return Response(payload, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
